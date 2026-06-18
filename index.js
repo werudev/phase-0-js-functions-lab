c43b63c1db23aac7db6798b48acbd1b9be175f29
werudev <weru.dennis@student.moringaschool.com>
2026-06-18 17:09:44 +0300

    initial css compulsory  commit

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
+ 
 
+function calculateTax(amount){
+    
+    const  taxRate = 0.10;
+    const  taxValue = taxRate * amount;
+ 
+    return(taxValue)
+ }
+
+ function convertToUpperCase(text){
+    const string = text.toUpperCase();
+
+    return (string)
+ }
+
+function findMaximum(num1 , num2){
+     
+    const  maxNum = Math.max(num1, num2)
+    return(maxNum)
+}
+
+function isPalindrome(word) {
+
+  const cleanedWord = word.toLowerCase();
+  const reversedWord = cleanedWord.split('').reverse().join('');
+
+  return cleanedWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    
+    const discountRate = discountPercentage / 100;
+
+    const discountAmount = originalPrice * discountRate;
+
+    const finalPrice = originalPrice - discountAmount;
+
+    return finalPrice; 
+}
 
 
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase , findMaximum, isPalindrome, calculateDiscountedPrice };
